25c767e4ea4590a3519c7c589fcfb618a9005b35
brianavid <collins.bj@gmail.com>
2017-03-20 16:20:35 +0000

    Re-work Chord to allow negative harmony intervals
    This allows Arpeggios to be able to implement Ornaments

--- src/com/brianavid/euterpea/Chord.scala
+++ src/com/brianavid/euterpea/Chord.scala
@@ -3,8 +3,10 @@ package com.brianavid.euterpea
 //  A Harmony is a set of intervals, with the lowest interval being the root of the harmony
 //  The harmony can be relative to a specific root note or can be relative to the current
 //  tonic (often the key, but may be modulated to a different tonic root)
+//  The root is a member of the intervalSet that is the root of the harmonic triad, which (if non-zero)
+//  will be different from the current tonic or whatever note is used as the base of a Chord
 
-case class Harmony(intervalSet: Set[Int])
+case class Harmony(intervalSet: Set[Int], val root: Int = 0)
 {
   //  Although the Harmony is a set of intervals, for inversions, arpeggios etc we need them sorted low to high
   def intervals = intervalSet.toIndexedSeq.sorted
@@ -24,25 +26,26 @@ case class Harmony(intervalSet: Set[Int])
   def c = inv(2)
   def d = inv(3)
   
-  //  What is the root (lowest) note of the Harmony
-  def root = intervalSet.min
-  
   //  Modify the Harmony by raising or lowering the fifth or third or adding a seventh  
-  def aug = Harmony(intervalSet-(root+7)+(root+8))
-  def dim = Harmony(intervalSet-(root+7)+(root+6))
-  def maj = Harmony(intervalSet-(root+3)+(root+4))
-  def min = Harmony(intervalSet-(root+4)+(root+3))
-  def sus = Harmony(intervalSet-(root+3)-(root+4)+(root+5))
-  def sus2 = Harmony(intervalSet-(root+4)-(root+3)+(root+2))
-  def add6 = Harmony(intervalSet + (root+9))
-  def min7 = Harmony(intervalSet + (root+10))
-  def maj7 = Harmony(intervalSet + (root+11))
+  def aug = copy(intervalSet=intervalSet-(root+7)+(root+8))
+  def dim = copy(intervalSet=intervalSet-(root+7)+(root+6))
+  def maj = copy(intervalSet=intervalSet-(root+3)+(root+4))
+  def min = copy(intervalSet=intervalSet-(root+4)+(root+3))
+  def sus = copy(intervalSet=intervalSet-(root+3)-(root+4)+(root+5))
+  def sus2 = copy(intervalSet=intervalSet-(root+4)-(root+3)+(root+2))
+  def add6 = copy(intervalSet=intervalSet + (root+9))
+  def min7 = copy(intervalSet=intervalSet + (root+10))
+  def maj7 = copy(intervalSet=intervalSet + (root+11))
+  def add8 = copy(intervalSet=intervalSet + (root+12))
+  def add9 = copy(intervalSet=intervalSet + (root+14))
+  def add11 = copy(intervalSet=intervalSet + (root+17))
+  def add13 = copy(intervalSet=intervalSet + (root+21))
   
   //  Transpose the Harmony up or down a semitone or an octave
-  def flat = Harmony(intervalSet.map(_-1))
-  def sharp = Harmony(intervalSet.map(_+1))
-  def unary_- = Harmony(intervalSet.map(_-12))
-  def unary_+ = Harmony(intervalSet.map(_+12))
+  def flat = Harmony(intervalSet.map(_-1), root-1)
+  def sharp = Harmony(intervalSet.map(_+1), root+1)
+  def unary_- = Harmony(intervalSet.map(_-12), root-12)
+  def unary_+ = Harmony(intervalSet.map(_+12), root+12)
 }
 
 //  The Harmony object defines scale intervals and chord shapes
@@ -64,12 +67,14 @@ object Dom7 extends Harmony(Harmony.triad(1).map(Harmony.majorIntervals) ++ Harm
 object Maj7 extends Harmony(Harmony.triad(1).map(Harmony.majorIntervals) ++ Harmony.seventh(1).map(Harmony.majorIntervals))
 object Min7 extends Harmony(Harmony.triad(1).map(Harmony.minorIntervals) ++ Harmony.seventh(1).map(Harmony.minorIntervals))
 
+//  A Chord can be broken by the addition of a Broken modifier, which specifies note spacing
 case class Broken( 
-    val delay: Double)
+    val delay: Double)  //  The delay between the onset of each note in the chord
 
+//  A Chord can become an Arpeggio by the addition of an Arpeggio modifier, which specifies the beat and note pattern sequence
 case class Arpeggio( 
-    val beat: Beat, 
-    val sequence: Vector[Int])
+    val beat: Beat,             //  The beat of each note in the arpeggio
+    val sequence: Vector[Int])  //  The sequence of the Chord's notes (low to high, 1-based, 0=Rest) 
 
 //  A chord is a Harmony with either an explicit root note or which is in a position relative to the current tonic 
 //  A chord contains a list of transforms that can be applied to the harmony e.g. to add and remove intervals
@@ -78,17 +83,17 @@ case class Chord(
     harmony: Harmony,             //  An explicit harmony
     chordPosition: Int = 1,   //  If the root is None, the chord position relative to the current tonic 
     addSeventh: Boolean = false,  //  Add a seventh to the chord?
-    brokenDelay: Option[Double] = None,  //  For a broken chord, the delay between the onset of each note in the chord
-    arpeggioPattern: Option[Arpeggio] = None,  //  For an arpeggio or ornament, the pattern (sequence and timing) 
+    broken: Option[Broken] = None,  //  For a broken chord, the delay between the onset of each note in the chord
+    arpeggio: Option[Arpeggio] = None,  //  For an arpeggio or ornament, the pattern (sequence and beat) 
     transforms: List[Harmony => Harmony] = Nil) extends Music 
 {
   //  Make the chord a broken cord, adding a fraction of a beat delay to each note in the chord
-  def / (b: Broken) = copy(brokenDelay=Some(b.delay))
-  def /: (b: Broken) = copy(brokenDelay=Some(b.delay))
+  def / (b: Broken) = copy(broken=Some(b))
+  def /: (b: Broken) = copy(broken=Some(b))
     
   //  Make the chord an arpeggiated cord, 
-  def / (a: Arpeggio) = copy(arpeggioPattern=Some(a))
-  def /: (a: Arpeggio) = copy(arpeggioPattern=Some(a))
+  def / (a: Arpeggio) = copy(arpeggio=Some(a))
+  def /: (a: Arpeggio) = copy(arpeggio=Some(a))
     
   //  Add the Chord to the current sequence so that all the notes (transposed by the Harmony intervals) 
   //  sound at the same time.
@@ -107,10 +112,12 @@ case class Chord(
         val intervals = if (context.isMinor) Harmony.minorIntervals else Harmony.majorIntervals
         
         //  Construct a base harmony from the chord position and tonic intervals, adding a dominant seventh if specified 
+        val triad =  Harmony.triad(chordPosition).map(intervals)
+        val root = triad.min
         if (addSeventh) 
-          (context.tonic, Harmony(Harmony.triad(chordPosition).map(intervals) ++ Harmony.seventh(chordPosition).map(Harmony.minorIntervals)))
+          (context.tonic, Harmony(triad ++ Harmony.seventh(chordPosition).map(Harmony.minorIntervals), root))
         else
-          (context.tonic, Harmony(Harmony.triad(chordPosition).map(intervals)))
+          (context.tonic, Harmony(triad, root))
       }
     }
     
@@ -132,39 +139,51 @@ case class Chord(
     
     //  What notes does the Chord play? Transpose the root note up by each of the effective Harmony intervals
     val notes = effectiveHarmony.intervals.map(rootNote/Transpose(_))
-      
-    arpeggioPattern match
+    
+    //  Does the Chord have an Arpeggio modifier, to play the Chord's note as a sequence?
+    arpeggio match
     {
-      case Some(pattern) =>
+      case Some(Arpeggio(beat, sequence)) =>
       {
-        val patternSequenceBeatRatio = context.durationTiming(0).ticks / pattern.beat.beatTicks
-        val patternSequenceCount = pattern.sequence.length min patternSequenceBeatRatio
-        pattern.sequence.take(patternSequenceCount-1).zipWithIndex.map{
+        //  How many arpeggiated notes can play in the duration of the note?
+        val patternSequenceBeatRatio = context.durationTiming(0).ticks / beat.beatTicks
+        
+        //  How many notes in the pattern sequence will play?
+        val patternSequenceCount = sequence.length min patternSequenceBeatRatio
+        
+        //  For all notes up to (but not including) that patternSequenceCount limit, play each note, 
+        //  each with an ever-increasing start position based on the arpeggio beat
+        sequence.take(patternSequenceCount-1).zipWithIndex.map{
           case (index: Int, i: Int) => 
+            //  If the note index in the sequence is out of range (e.g. zero) add a Rest
             if (index <= 0 || index > notes.length)
-              Rest.add(context.copy(beat=pattern.beat, position=context.position+Timing(pattern.beat * i, 1)))
+              Rest.add(context.copy(beat=beat, position=context.position+Timing(beat * i, 1)))
             else
-              notes(index-1).add(context.copy(beat=pattern.beat, position=context.position+Timing(pattern.beat * i, 1)))
+              //  Otherwise play the indexed note from those in the chord (indexed low-to-high, 1-based)
+              notes(index-1).add(context.copy(beat=beat, position=context.position+Timing(beat * i, 1)))
         }
         
+        //  For the last note in the pattern, also play it with an appropriate delay,
+        //  but with a duration of the rest of the Chord's beat
         val lastInPattern = patternSequenceCount-1
-        val remainingTiming = context.durationTiming(0).ticks - (pattern.beat.beatTicks * lastInPattern)
-        val index = pattern.sequence(patternSequenceCount-1)
+        val remainingTiming = context.durationTiming(0).ticks - (beat.beatTicks * lastInPattern)
+        val index = sequence(patternSequenceCount-1)
         if (index <= 0 || index > notes.length)
-          Rest.add(context.copy(beat=new Beat(remainingTiming), position=context.position+Timing(pattern.beat * lastInPattern, 1)))
+          Rest.add(context.copy(beat=new Beat(remainingTiming), position=context.position+Timing(beat * lastInPattern, 1)))
         else
-          notes(index-1).add(context.copy(beat=new Beat(remainingTiming), position=context.position+Timing(pattern.beat * lastInPattern, 1)))
+          notes(index-1).add(context.copy(beat=new Beat(remainingTiming), position=context.position+Timing(beat * lastInPattern, 1)))
 
+        //  The length of the arpeggiated Chord remains the same as though not arpeggiated
         context.durationTiming(0) * context.scaleBeats / context.scaleNum
       }
       
       case None =>
       {
         //  For a broken chord, each note has a delay (increasing as you go up the chord)
-        val delayedNotes = brokenDelay match
+        val delayedNotes = broken match
         {
-          case None => notes
-          case Some(delay) => 
+          case None => notes  //  I.e. NOT broken
+          case Some(Broken(delay)) => 
             {
               notes.zipWithIndex.map{
                 case (m:Music,i: Int) => (WithDynamics(Dynamics.delay(context.beat, delay*i), m))
@@ -194,6 +213,10 @@ case class Chord(
   def dom7 = copy(transforms = ((h: Harmony) => h.min7) :: transforms)
   def min7 = copy(transforms = ((h: Harmony) => h.min7) :: transforms)
   def maj7 = copy(transforms = ((h: Harmony) => h.maj7) :: transforms)
+  def add8 = copy(transforms = ((h: Harmony) => h.add8) :: transforms)
+  def add9 = copy(transforms = ((h: Harmony) => h.add9) :: transforms)
+  def add11 = copy(transforms = ((h: Harmony) => h.add11) :: transforms)
+  def add13= copy(transforms = ((h: Harmony) => h.add13) :: transforms)
   def flat = copy(transforms = ((h: Harmony) => h.flat) :: transforms)
   def sharp = copy(transforms = ((h: Harmony) => h.sharp) :: transforms)
   def unary_- = copy(transforms = ((h: Harmony) => -h) :: transforms)
@@ -238,12 +261,12 @@ object Chord
     val fromNoteTonicIndex = intervals.indexWhere(_ >= noteTonicOffset)
     
     //  And so where in the scale intervals is the note to which it will be transposed
-    //  Note that this may be less than 0 and so must be octave adjusted
     val toNoteTonicIndex = fromNoteTonicIndex + toChord.chordPosition - fromChord.chordPosition
     
     //  Transpose the note by adding and subtracting the found diatonic intervals
-    if (fromNoteTonicIndex <= 0)
-      semitones - 12 + intervals(toNoteTonicIndex) - intervals(fromNoteTonicIndex + 7)
+    //  Note that the difference may be less than 0 and so must be octave adjusted
+    if (toNoteTonicIndex < 0)
+      semitones - 12 + intervals(toNoteTonicIndex+7) - intervals(fromNoteTonicIndex)
     else
       semitones + intervals(toNoteTonicIndex) - intervals(fromNoteTonicIndex)
   }
