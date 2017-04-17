a8d0785788e1f8eacc7a22a1686b3d815f75f93b
brianavid <collins.bj@gmail.com>
2017-04-17 15:49:34 +0100

    Add relative Volume modifiers

--- src/com/brianavid/euterpea/Volume.scala
+++ src/com/brianavid/euterpea/Volume.scala
@@ -2,10 +2,10 @@ package com.brianavid.euterpea
 
 //  The Volume Modifier controls the volumes (actually velocities) of notes
 
-case class Volume(val volume: Int) extends Modifier
+case class Volume(volume: Int, isRelative: Boolean = false) extends Modifier
 {
   def modifying(music: Music): Music =
-    new WithVolume (volume, music)
+    new WithVolume (volume, isRelative, music)
 
   def -> (toVolume: Volume) = new VolumeChange(this, toVolume)
 }
@@ -17,6 +17,8 @@ object Volume
   val DefaultVolume = 68
   val VolumeInc = 15
 }
+
+//  Absolute Volume Mofier objects
 object Vmf extends Volume(Volume.DefaultVolume)                       //  Normal (default) volume
 object Vmp extends Volume(Volume.DefaultVolume  - Volume.VolumeInc)   //  A biq quieter than normal volume
 object Vf extends Volume(Volume.DefaultVolume + Volume.VolumeInc)     //  Louder (forte)
@@ -26,24 +28,33 @@ object Vp extends Volume(Volume.DefaultVolume - Volume.VolumeInc)     //  Quiete
 object Vpp extends Volume(Volume.DefaultVolume - Volume.VolumeInc*2)  //  Even quieter
 object Vppp extends Volume(Volume.DefaultVolume - Volume.VolumeInc*3) //  Even quieter still
 
+//  Relative Volume Modifier objects
+object Vcurrent extends Volume(0, true)
+object Vlouder extends Volume(Volume.VolumeInc, true)
+object Vlouder2 extends Volume(Volume.VolumeInc*2, true)
+object Vlouder3 extends Volume(Volume.VolumeInc*3, true)
+object Vquieter extends Volume(-Volume.VolumeInc, true)
+object Vquieter2 extends Volume(-Volume.VolumeInc*2, true)
+object Vquieter3 extends Volume(-Volume.VolumeInc*3, true)
+
 //  The VolumeChange Modifier increases or decreases Volume gradually over the duration of the music
 
 private[euterpea] case class VolumeChange(val fromVolume: Volume, val toVolume: Volume) extends Modifier
 {
     def modifying(music: Music): Music =
-      new WithVolumeChange(toVolume.volume-fromVolume.volume, 
-                           new WithVolume(fromVolume.volume, music))
+      new WithVolumeChange(if (toVolume.isRelative) toVolume.volume else toVolume.volume-fromVolume.volume, 
+                           new WithVolume(fromVolume.volume, fromVolume.isRelative, music))
 }
 
 //-------------------------
 
 //  Add the music, with a changed current note volume
 
-private[euterpea] case class WithVolume(volume: Int, music: Music) extends Music
+private[euterpea] case class WithVolume(volume: Int, isRelative: Boolean, music: Music) extends Music
 {
   def add(context: SequenceContext) =
   {
-    music.add(context.copy(volume=volume))
+    music.add(context.copy(volume=if (isRelative) (context.volume+volume max 0 min 127) else volume))
   }
   
   def duration(context: SequenceContext) = music.duration(context)
